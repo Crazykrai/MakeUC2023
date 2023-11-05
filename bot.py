f7e5c6c8572d98daf59f57867779cf7f985f6aec
Tucker Cook <jctuck333@gmail.com>
2023-11-04 21:11:03 -0400

    big things happening

--- bot.py
+++ bot.py
@@ -2,14 +2,33 @@ import os
 
 import discord
 from dotenv import load_dotenv
-
-from cocimands import *
+from discord.ext import commands
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
+intents = discord.Intents.all()
+intents.members = True
+CociBot = commands.Bot(command_prefix="//", intents=intents)
+
 @CociBot.event
 async def on_ready():
     print(f'{CociBot.user.name} has connected to Discord!')
+    channel = discord.utils.get(CociBot.get_all_channels(), name="General") 
+    await channel.connect()
+
+@CociBot.command()
+async def info(ctx):
+    await ctx.send("You're a livesaver!")
+
+@CociBot.command(pass_context = True)
+async def join(ctx):
+    channel = discord.utils.get(CociBot.get_all_channels(), name="General")
+    await channel.connect()
+
+@CociBot.command(pass_context = True)
+async def leave(ctx):
+    if (ctx.voice_client):
+        await ctx.guild.voice_client.disconnect()
 
 CociBot.run(TOKEN)
--- voiceInput.py
+++ voiceInput.py
@@ -1,3 +1,17 @@
-import sounddevice as sd
-from scipy.io.wavfile import write
-import wavio as wv
+import speech_recognition as sr
+import re
+
+# obtain audio from the microphone
+r = sr.Recognizer()
+
+with sr.Microphone() as source:
+    print("Say something!")
+    audio = r.listen(source)
+
+OPENAI_API_KEY = "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"
+
+try:
+    print(f"Whisper API thinks you said {r.recognize_whisper_api(audio, api_key=OPENAI_API_KEY)}")
+except sr.RequestError as e:
+    print("Could not request results from Whisper API")
+
